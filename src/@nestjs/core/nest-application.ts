b66a0a187713da37a1c09ad651454516c448267b
enson_chen <1004714132@qq.com>
2025-07-11 13:32:40 +0800

    feat: 在 package.json 中添加开发模式命令并更新 express 版本

--- src/@nestjs/core/nest-application.ts
+++ src/@nestjs/core/nest-application.ts
@@ -1,4 +1,5 @@
-import express, { Express } from 'express';
+import * as express from 'express';
+import { Express } from 'express';
 
 export class NestApplication {
   private readonly app: Express = express();
